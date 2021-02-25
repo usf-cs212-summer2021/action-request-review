170090cc981e453eb312abfedcf0705d0abc587b
Sophie Engle <sjengle@cs.usfca.edu>
2021-02-24 16:00:15 -0800

    Adding cache package

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
+const cache = require('@actions/cache');
 const style = require('ansi-styles');
 
 exports.warnings = 0; // track warnings
